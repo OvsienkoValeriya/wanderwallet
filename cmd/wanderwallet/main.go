f2ce8cdb1f7dcd077fb9bc7a8986c18157199e04
OvsienkoValeriya <44091861+ovsienkovaleriya@users.noreply.github.com>
2025-09-09 13:54:12 +0200

    graceful shutdown added

--- cmd/wanderwallet/main.go
+++ cmd/wanderwallet/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 	"wanderwallet/initializers"
 	"wanderwallet/internal/controllers"
 	"wanderwallet/internal/middleware"
@@ -79,7 +84,34 @@ func main() {
 			analyticsRoutes.GET("", analyticsController.GetAnalytics)
 		}
 	}
-	if err := r.Run(); err != nil {
-		log.Fatalf("Server run failed: %v", err)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
 	}
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server run failed: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting gracefully")
 }
